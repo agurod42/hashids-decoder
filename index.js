0ba34269bcccd871c5c7f40c030b6f45fb5b0773
Agustín Rodríguez <me@agurodriguez.net>
2019-04-22 23:53:25 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const Hashids = require('hashids');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    if (!req.query.id) return res.sendStatus(400);
+    if (!req.query.salt) return res.sendStatus(400);
+
+    const hashids = new Hashids(req.query.salt, req.query.length);
+    res.send(hashids.decode(req.query.id));
+});
+
+app.listen(process.env.PORT || 4000, function () {
+    console.log('hashids-decoder listening on port 4000!');
+});
