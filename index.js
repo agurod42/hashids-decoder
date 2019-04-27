e8cfbaa60f90baf176be4e43065c584772e580b7
Agustín Rodríguez <me@agurodriguez.net>
2019-04-27 14:30:47 -0400

    Add /encode endpoint

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const Hashids = require('hashids');
 
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/decode', function (req, res) {
     if (!req.query.id) return res.sendStatus(400);
     if (!req.query.salt) return res.sendStatus(400);
 
     const hashids = new Hashids(req.query.salt, req.query.length);
     res.send(hashids.decode(req.query.id));
+});
+
+app.get('/encode', function (req, res) {
+    if (!req.query.hash) return res.sendStatus(400);
+    if (!req.query.salt) return res.sendStatus(400);
+
+    const hashids = new Hashids(req.query.salt, req.query.length);
+    res.send(hashids.encode(req.query.hash));
 });
 
 app.listen(process.env.PORT || 4000, function () {
